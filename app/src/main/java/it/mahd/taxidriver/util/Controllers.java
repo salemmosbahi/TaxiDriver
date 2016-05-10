07d1dafa7e49b54514d9a9a8c439ed8e7a02aeb1
Salem MOSBAHI <salemmosbahi@gmail.com>
2016-05-10 17:04:25 +0100

    modifi control

--- app/src/main/java/it/mahd/taxidriver/util/Controllers.java
+++ app/src/main/java/it/mahd/taxidriver/util/Controllers.java
@@ -9,7 +9,7 @@ import android.net.NetworkInfo;
  */
 public class Controllers {
     //public static final String url = "http://10.0.2.2:4004";
-    public static final String url = "http://192.168.1.3:4004";
+    public static final String url = "http://192.168.43.71:4004";
     public static final String url_addReclamation = url + "/addReclamation";
     public static final String url_getAllReclamation = url + "/getAllReclamation";
     public static final String url_addMessage = url + "/addMessage";
